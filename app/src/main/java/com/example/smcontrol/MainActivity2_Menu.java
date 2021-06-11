f0c7b1d602cb2f83231c2f456f8c9577261bb546
serg1o99 <eldady_elsuperio@hotmail.com>
2021-06-11 02:57:22 -0500

    4to commit (Actualizado todo lo pendiente diseño del login y el activity menu  )

---
+++ app/src/main/java/com/example/smcontrol/MainActivity2_Menu.java
@@ -0,0 +1,14 @@
+package com.example.smcontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2_Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2__menu);
+    }
+}
