c61eeee60f7b9d185ecb7253809d2e840bd7603d
Anderson <andersonsurco5@gmail.com>
2021-06-15 15:53:50 -0500

    activiy trabajador, producto, categoria, proveedor y reporte

--- app/src/main/java/com/example/smcontrol/MenuActivity.java
+++ app/src/main/java/com/example/smcontrol/MenuActivity.java
@@ -1,14 +1,61 @@
 package com.example.smcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public CardView cardTrabajador, cardProducto, cardCategoria, cardProveedor,  cardReporte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        cardTrabajador = (CardView) findViewById(R.id.cardTrabajador);
+        cardProducto = (CardView) findViewById(R.id.cardProducto);
+        cardCategoria = (CardView) findViewById(R.id.cardCategoria);
+        cardProveedor = (CardView) findViewById(R.id.cardProveedor);
+        cardReporte = (CardView) findViewById(R.id.cardReporte);
+
+        cardTrabajador.setOnClickListener(this);
+        cardProducto.setOnClickListener(this);
+        cardCategoria.setOnClickListener(this);
+        cardProveedor.setOnClickListener(this);
+        cardReporte.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent i;
+
+        switch (v.getId()){
+            case R.id.cardTrabajador:
+                i = new Intent(this,TrabajadorActivity.class);
+                startActivity(i);
+                break;
+            case R.id.cardProducto:
+                i = new Intent(this, ProductoActivity.class);
+                startActivity(i);
+                break;
+            case R.id.cardCategoria:
+                i = new Intent(this, CategoriaActivity.class);
+                startActivity(i);
+                break;
+            case R.id.cardProveedor:
+                i = new Intent(this, ProveedorActivity.class);
+                startActivity(i);
+                break;
+            case R.id.cardReporte:
+                i = new Intent(this, ReporteActivity.class);
+                startActivity(i);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/smcontrol/TrabajadorActivity.java
@@ -0,0 +1,14 @@
+package com.example.smcontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrabajadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trabajador);
+    }
+}
